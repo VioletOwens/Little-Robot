46beecd86d98b512c4632dce4907d938830308ad
Violet <VioletmOwens0@gmail.com>
2021-11-23 12:02:27 -0600

    Formatting the files

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 		@Override
 		   public void start(Stage stage) throws Exception {
 				try {
-				URL fxmlResource = getClass().getResource("/fxmls/Main.fxml");
+				URL fxmlResource = getClass().getResource("/Main.fxml");
 				//URL fxmlURL = getClass().getResource("/Little Robot/fxmls/Main.fxml");
 				Parent root = FXMLLoader.load(fxmlResource);
 				//Parent root = FXMLLoader.load(getClass().getResource("C:\\Users\\chris\\Desktop\\CS\\CS Software\\Workspace\\Little Robot\\fxmls\\Main.fxml"));
